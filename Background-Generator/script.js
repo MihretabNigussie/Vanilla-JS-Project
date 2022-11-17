24f9fdf1f5dad766c48a75c37b72d308d57840b3
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2022-11-17 09:21:07 +0300

    Add files via upload

---
+++ Background-Generator/script.js
@@ -0,0 +1,13 @@
+let color1 = document.getElementById("color1");
+let color2 = document.getElementById("color2");
+let css = document.querySelector("h3");
+let body = document.body;
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right ," + color1.value + " , " +  color2.value + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
